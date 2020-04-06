15ce89ada9214356be54caa5221e89b47ba2143d
Devon DeJohn <ddejohn@pm.me>
2020-04-06 11:06:09 -0600

    prolog done, still sucks

--- climbers.pl
+++ climbers.pl
@@ -18,12 +18,8 @@ certified(maria).
 certified(derek).
 not(certified(thyago)).
 
-
-member(X, [X | _]).
-member(X, [_ | T]) :- member(X, T).
-
 will_climb(P, M) :- certified(P), climber(P), (
-    P == maria;
+    P == maria, mountain(M, _, _);
     P == john, mountain(M, C, _), C==north_america;
     P == kelly, mountain(M, _, H), H >= 20000;
     P == derek, mountain(M, C, H), H =< 19000, (
@@ -35,21 +31,7 @@ will_climb(P, M) :- certified(P), climber(P), (
 
 is_mountain(M) :- mountain(M, _, _).
 taller_than(M, X) :-
-    not(mountain(X, _, H)), mountain(M, _, H), H > X;
-    mountain(X, _, H), taller_than(M, H).
+    mountain(X, _, H), taller_than(M, H);
+    integer(X), mountain(M, _, H), H > X.
 located_in(M, X) :- mountain(M, C, _), C == X.
-
-% map(F, X) :- F(X)
-
-
-% Is Mt. Everest one of the highest peaks in the world? true
-% Is the Kilimanjaro located in Africa? true
-%  Is Mt. Elbrus more than 18K feet? true
-% Is Thyago a certified climber? false
-% Is John a certified climber? true
-% All certified climbers? John, Kelly, Maria, and Derek
-% All peaks that John would climb?  Mt. McKinley
-% All peaks that Kelly would climb? Mt. Everest, the Aconcagua, and Mt. McKinley
-% All peaks that Maria would climb? all of the 7 peaks!
-% All peaks that Derek would climb? Mt. Elbrus and the Puncak Jaya
-% All peaks that Thyago would climb? none (or false)
+all_peaks(P) :- (will_climb(P, M), mountain(M, _, _)), write(M).
